24b276abc63604482260d65fc20bd44f9e28dfc2
petermainakihara <pkihara648@gmail.com>
2025-07-27 22:51:58 +0300

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I enjoy building creative web applications and solving problems.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
